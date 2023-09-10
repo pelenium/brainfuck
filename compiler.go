c29f2d4bc90cc12ef48f7efa33ac3082a5ad72c5
PELENIUM <i.d.chernyshov@yandex.ru>
2023-09-10 18:43:17 +0300

    fixed a bug with ptr

--- compiler.go
+++ compiler.go
@@ -7,9 +7,9 @@ import (
 func fuckBrain(source string) {
 	arr := make([]byte, 300000)
 
-	ptr := 0
+	ptr := 15000
 
-	s := []int{}
+	stack := []int{}
 	index := 0
 	for index < len(source) {
 		i := source[index]
@@ -32,7 +32,7 @@ func fuckBrain(source string) {
 				if ptr > 0 {
 					ptr--
 				} else {
-					fmt.Println("Error: pointer incrementing error: bounds out of range")
+					fmt.Println("Error: pointer decrementing error: bounds out of range")
 					return
 				}
 			case "+":
@@ -40,12 +40,25 @@ func fuckBrain(source string) {
 			case "-":
 				arr[ptr]--
 			case "[":
-				s = append(s, index-1)
+				if arr[ptr] == 0 {
+					loopCounter := 1
+					for loopCounter > 0 {
+						index++
+						if string(source[index]) == "[" {
+							loopCounter++
+						} else if string(source[index]) == "]" {
+							loopCounter--
+						}
+					}
+				} else {
+					stack = append(stack, index)
+				}
 			case "]":
 				if arr[ptr] != 0 {
-					index = s[len(s)-1]
+					index = stack[len(stack)-1]
+				} else {
+					stack = stack[:len(stack)-1]
 				}
-				s = s[:len(s)-1]
 			}
 		}
 		index++
